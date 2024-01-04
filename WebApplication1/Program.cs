4d44efe3cdc0c5200ac71fb18f794206f8c95dc0
Nicovied0 <briannoviedo@gmail.com>
2024-01-04 17:32:14 -0300

    second commit

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -21,11 +21,14 @@ builder.Services.AddControllers().AddJsonOptions(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
